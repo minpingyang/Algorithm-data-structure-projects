3836dc0fea92a330e46873fee72f43ce31f23f1e
hic <minping.yang@yahoo.com>
2017-03-10 00:55:47 +1300

    write the polygon to try to make the map looks beautiful

--- AklRoadMap-project/Polygons.java
+++ AklRoadMap-project/Polygons.java
@@ -1,5 +1,32 @@
 package code.comp261.ass1;
 
-public class Polygons {
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Set;
 
+// Polygon class is used to make the road map beatiful
+
+public class Polygons {
+	public final String label;
+	public final Integer type;
+	public final Integer endLevel;
+	public final Integer cityId;
+	
+	public final Set<ArrayList<Location>> location;
+	private final Color color;
+	
+	public Polygons (Integer type, String label, Integer endLevel,Integer cityId, Set<ArrayList<Location>> location ){
+		
+		this.type = type;
+		this.label = label;
+		this.endLevel = endLevel ;
+		this.cityId = cityId ;
+		this.location = location;
+		swith (this.type){
+		case 0x07:
+				
+		}
+	}
+	
+	
 }
