45aa13454aca73a41691e11fa4cb49875f7d40c4
hic <minping.yang@yahoo.com>
2017-03-12 01:22:30 +1300

    12Mar midnight

--- AklRoadMap-project/AucklandRoadMap.java
+++ AklRoadMap-project/AucklandRoadMap.java
@@ -1,28 +1,86 @@
 package code.comp261.ass1;
 
+import java.awt.Graphics;
+import java.awt.List;
+import java.awt.event.MouseEvent;
 import java.io.File;
 /**
  * 
  * 
  * 
- * 
+ * @author minping_yang
  * 
  * */
+import java.util.ArrayList;
+import java.util.Set;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
 public class AucklandRoadMap extends GUI {
 	
 	// the center of Auckland City according to Location class
-		private static final double CENTRE_LAT = -36.847622;
-		private static final double CENTRE_LON = 174.763444;
+	private static final double CENTRE_LAT = -36.847622;
+	private static final double CENTRE_LON = 174.763444;
+	private double scale;
+	private static final double zoom_factor = 1.4;
+	private static final double minimun = 7, maximum = 1000;
+	private static final Location centre =Location .newFromLatLon(CENTRE_LAT, CENTRE_LON );
+	private Location origin;
+	private ArrayList<Roads> matchingRoads; // these roads are matched what the user typed in.
+	// private Location locationRoadStartDrag;
+	private Node startNode;
+	private Node endNode;
+	private ArrayList<RoadSegment> path;
 	
+	//declaration of articulation nodes
+	private Set<Node> articulationNodes;
+	private boolean doesShowArticulationPoints;
 	
-	public static void main(String[] args) {
+		
+		
+public static void main(String[] args) {
 		new AucklandRoadMap();		
 	}
-	public 
+
+
+
+@Override
+protected void redraw(Graphics g) {
+	// TODO Auto-generated method stub
 	
-    protected void onload(File nodes, File roads, File segments, File polygons, File restrictions){
-    	
-    	
-    	
-    }
+}
+
+
+
+@Override
+protected void onClick(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+
+
+@Override
+protected void onSearch() {
+	// TODO Auto-generated method stub
+	
+}
+
+
+
+@Override
+protected void onMove(Move m) {
+	// TODO Auto-generated method stub
+	
+}
+
+
+
+@Override
+protected void onLoad(File nodes, File roads, File segments, File polygons) {
+	// TODO Auto-generated method stub
+	
+}
+	
+	
+  
 }
