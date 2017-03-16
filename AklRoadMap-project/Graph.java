033fc7a55c82e0296d03e3c9201eeca834c9f68b
hic <minping.yang@yahoo.com>
2017-03-17 01:05:52 +1300

    done partly graph class wrote a wrapper loadFile method,etc

--- AklRoadMap-project/Graph.java
+++ AklRoadMap-project/Graph.java
@@ -1,78 +1,114 @@
 package code.comp261.ass1;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.nio.channels.FileLockInterruptionException;
+import javax.swing.text.Segment;
+import java.io.*;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+/*
+* Graph class means the graph of linking nodes and roads
+* @author Minping
+* */
 public class Graph {
-	public static final double MAX_CLICKED_RANGE = 0.18;    // how far away of clicked location can be counted (0.18 JUST GUESS, PROBABLY CHANGE IT LATER
-	// create collections to store node
-	Map<Integer,Node> nodeMap;
-	Map<Integer, Roads> roadMap;  //store roads;
-	Set<RoadSegment> segments;   // store seg
-	Set<Polygons> polygons;     // store polyg
-	Set<Singularity> singularities;   // singularity points of the map
-	RoadTrie roadTrie;
-	public Graph(){
-		nodeMap =new HashMap<>();
-		roadMap = new HashMap<>();
-		polygons = new HashSet<>();
-		segments = new HashSet<>();
-		singularities = new HashSet<>();
-		roadTrie = new RoadTrie();
-	}
-	public void load(File nodeFile, File roadFile, File segmentFile, File polygonFile, File singularityFile){
-		nodeMap = new HashMap<>();
-		roadMap = new HashMap<>();
-		polygons = new HashSet<>();
-		segments = new HashSet<>();
-		singularities = new HashSet<>();
-		roadTrie = new RoadTrie();
-		// declare corresponding function to load different data
-		loadNodes(nodeFile);
-		loadRoads(roadFile);
-		loadSegments(segmentFile);
-		
-		if (polygonFile== null)  return;
-		loadPolygons(polygonFile);
-	    if (singularityFile==null)  return;
-	    loadSingularity(singularityFile);		
-	}
-	private void loadSingularity(File singularityFile) {
-		BufferedReader bufferedReader;
-		try{
-			bufferedReader = new BufferedReader(new FileReader(file));
-			bufferedReader.readLine(); //ignore first line 
-			String line =bufferedReader.readLine();
-			while (line!= null ){
-				Singularity singularity =new Singularity()
-			}
-		}
-		
-	}
-	private void loadPolygons(File polygonFile) {
-		// TODO Auto-generated method stub
-		
-	}
-	private void loadSegments(File segmentFile) {
-		// TODO Auto-generated method stub
-		
-	}
-	private void loadRoads(File roadFile) {
-		// TODO Auto-generated method stub
-		
-	}
-	private void loadNodes(File nodeFile) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	
-	
-	
+    Map<Integer,Node> nodeMap;
+    Map<Integer,Road> roadMap;
+    Set<Polygon> polygonSet;
+    Set<RoadSegment> segmentSet;
+
+    RoadTrie roadTrie;
+    public static final double CLICKED_RANGE = 0.2;
+
+    public Graph(){
+        nodeMap = new HashMap<>();
+        roadMap = new HashMap<>();
+        polygonSet = new HashSet<>();
+        roadTrie = new RoadTrie();
+    }
+    /**
+     * wrapper method for loading different files
+     * @param
+     * @param
+     * @param
+     * */
+    public void load(File nodes, File roads, File segments, File polygons){
+        // initialisation
+        nodeMap = new HashMap<>();
+        roadMap = new HashMap<>();
+        polygonSet = new HashSet<>();
+        roadTrie = new RoadTrie();
+
+
+        loadNodes(nodes);
+        loadRoads(roads);
+        loadSegments(segments);
+        //for security, since the user may click the small data folder
+        if(polygons== null) return;
+        loadPolygons(polygons);
+    }
+
+    private void loadNodes(File nodes) {
+        BufferedReader bufferedReader;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(nodes));
+            String line = bufferedReader.readLine();
+            while (line!=null){
+                Node node =new Node(line);
+                //node is indexed by its ID
+                nodeMap.put(node.nodeId,node);
+                line = bufferedReader.readLine(); // next line
+            }
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(nodes+ "File has not been found");
+        } catch (IOException e) {
+            System.out.print("IO Exception");
+        }
+    }
+    /*very similar to loadNode method
+    * */
+    private void loadRoads(File roads) {
+        BufferedReader bufferedReader;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(roads));
+            bufferedReader.readLine(); //skip first title line
+            String line = bufferedReader.readLine();
+            while (line!=null){
+                Road road =new Road(line);
+                //Road is indexed by its ID
+                nodeMap.put(road.roadId,road);
+                line = bufferedReader.readLine(); // next line
+            }
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(roads+ "File has not been found");
+        } catch (IOException e) {
+            System.out.print("IO Exception");
+        }
+
+    }
+    private void loadSegments(File segments) {
+        BufferedReader bufferedReader;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(segments));
+            bufferedReader.readLine(); // skip the first line
+            String line = bufferedReader.readLine();
+            while (line!=null){
+                RoadSegment segment =new RoadSegment(line, nodeMap, roadMap);
+                
+                //node is indexed by its ID
+                nodeMap.put(node.nodeId,node);
+                line = bufferedReader.readLine(); // next line
+            }
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(nodes+ "File has not been found");
+        } catch (IOException e) {
+            System.out.print("IO Exception");
+        }
+    }
+    private void loadPolygons(File polygons) {
+    }
+
 }
