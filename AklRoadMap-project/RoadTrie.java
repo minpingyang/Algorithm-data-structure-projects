46c1ece0c6e3d627039a6dd152b6954e0306ce99
hic <minping.yang@yahoo.com>
2017-03-16 13:54:26 +1300

    RoadTrie v2.0 finish the inner class and find,add methods so far, but need further test

--- AklRoadMap-project/RoadTrie.java
+++ AklRoadMap-project/RoadTrie.java
@@ -7,6 +7,9 @@ import java.util.*;
 * plan to use a nested-class structure,which is learnt from last year
 * internal class is RoadTrieNode i guess
 * outside class is RoadTrie.
+*
+*
+* author Minping
 * */
 public class RoadTrie {
 
@@ -23,24 +26,61 @@ public class RoadTrie {
             roads = new HashSet<>();
             children =new HashMap<>();
         }
-
-        /*Firstly, checking isMark(True) literally through for-LOOP
-        *
+        /*This method:
+        * Firstly,Find all nodes(isMarked=='true') of subtree of current node.
+        * then , add them into a list "roads".
         *
         * */
-        public void getAllFromNode(ArrayList<Road> roads){
+        public void getAllFromNode(List<Road> roads){
             //exit condition of recursion
             if(!isMarked || roads.isEmpty()) return;
-            roads.addAll(this.roads);
-            
+            roads.addAll(this.roads);  // 'this' represent which object call getAllFromNode
+            //use recursion to store
+            for(RoadTrieNode node: children.values()) {
+                node.getAllFromNode(roads);
+            }
+
         }
 
     }
 
-
+    // fileds of RoadTries class
     private  RoadTrieNode root;
     //constructor  EZ
     public RoadTrie(){
-        root = new Road
+        root = new RoadTrieNode();
     }
+    /***This method is used to add a road to RoadTrie data structure by
+    */
+    public void add(String string, Road road){
+        RoadTrieNode tempRootNode = root;
+        string = string.trim(); // trim() is used to returns a copy of the string, with leading and trailing whitespace omitted.
+        for(int i = 0 ; i < string.length(); i++){
+            char c = string.charAt(i);  // only add one character of string as a key step by step
+            if (tempRootNode ==null){ tempRootNode = tempRootNode.children.put(c,new RoadTrieNode())}
+            tempRootNode = tempRootNode.children.get(c);
+        }
+        tempRootNode.isMarked = true;
+        tempRootNode.roads.add(road);
+    }
+    public List<Road> find(String string){
+
+        RoadTrieNode temRootNode = root;
+        string = string.trim();
+        for (int i = 0; i<string.length(); i++){
+            char c = string.charAt(i);
+            if (temRootNode.children.get(c)!=null){
+                temRootNode.children.get(c);
+            }else{
+                return null;
+            }
+        }
+        //use list instead of set, since list store data orderly
+        List<Road> roads = new ArrayList<>();
+        temRootNode.getAllFromNode(roads);
+        return roads;
+
+    }
+
+
 }
