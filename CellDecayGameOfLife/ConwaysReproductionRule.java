ebe392467cb5ee00ec80287e894f6f200d25b3d2
hic <minping.yang@yahoo.com>
2017-03-21 13:54:40 +1300

    make reproducePopulationRule functional

---
+++ CellDecayGameOfLife/ConwaysReproductionRule.java
@@ -0,0 +1,24 @@
+package swen221.lab2.rules;
+
+import swen221.lab2.CellDecayGameOfLife;
+import swen221.lab2.model.*;
+import swen221.lab2.util.ConwayAbstractRule;
+
+/**
+ * This should implement Conway's rule for reproduction:
+ * 
+ * "Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction"
+ * 
+ * @author David J. Pearce
+ *
+ */
+public class ConwaysReproductionRule extends ConwayAbstractRule {
+	
+	@Override
+	public int apply(int x, int y, int neighbours) {
+		if(neighbours == 3){
+			return ConwayAbstractRule.board.getCellState(x, y) + GET_YOUNGER;		
+		}
+		return Rule.NOT_APPLICABLE;		
+	}
+}
