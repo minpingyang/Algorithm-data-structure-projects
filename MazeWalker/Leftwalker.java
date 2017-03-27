faa07df177edd0b9b55752d133f53bf9e861ecc4
hic <minping.yang@yahoo.com>
2017-03-28 12:30:56 +1300

    create changeCurrentDirection_toAavaiableDirection()  method and clockwiseFollowWall() method

--- MazeWalker/Leftwalker.java
+++ MazeWalker/Leftwalker.java
@@ -12,10 +12,10 @@ import java.util.List;
  * Created by minpingyang on 21/03/17.
  */
 public class Leftwalker extends Walker {
-    //the direction of walker heading in
+    //the currentDirection of walker heading in
     private Direction currentDirection;
 
-    private boolean isFrontWall, isRightWall,isLeftWall,isBackWall;
+    private boolean existForwardWall, existRightWall, existLeftWall, existBackWall;
     private boolean isFindingWall; // if the walker is looking for a wall on his left side
     private coordSystemBaseOnWalker coordinateOfwalker;
     //a map collection of visited square ---> list of directions of unvisited corresponding to this square
@@ -35,20 +35,54 @@ public class Leftwalker extends Walker {
             List<Direction> alternativeDirections = judgePotentialDirections(view);
             unvisitedDirections.put(coordinateOfwalker,alternativeDirections);
         }
-        resetDirections(view);
+        checkAllCurrentWallsStatus(view);
         if(isFindingWall)
-            lookLeftWall();
+            changeCurrentDirection_toAvaiableDirection();
         else
             clockwiseFollowWall();
 
         memorise();
         return currentDirection;
     }
-
     /**
-     * This method is used to get all potential available direction from walker view
+     * change currentDirection to avaiableDirection
+     * by searching left hand rule(clock-wise searching)
+     * ***/
+    private void changeCurrentDirection_toAvaiableDirection(){
+        isFindingWall = !existForwardWall && !existBackWall && !existLeftWall && !existLeftWall;
+        // front wall is available && left wall not available 
+        if(existLeftWall && !existForwardWall ){
+         //keep forward
+        }
+        //last case fail, then check if rightwall exist assuming forwardWall exist
+        else if(existForwardWall && !existRightWall){
+            //turn right
+            currentDirection = getDirectionValue(currentDirection,"right");
+        }//once last two case failed,
+        else if(existRightWall && !existBackWall){
+            currentDirection = getDirectionValue(currentDirection,"opposite");
+        }else if(existBackWall && !existLeftWall){
+            currentDirection =getDirectionValue(currentDirection,"left");
+        }
+    }
+    private void clockwiseFollowWall(){
+        if(!existLeftWall)
+            currentDirection = getDirectionValue(currentDirection,"left");
+        else if (!existForwardWall){
+
+        }
+        else if(!existRightWall){
+            currentDirection =getDirectionValue(currentDirection,"right");
+        }else if (!existBackWall){
+            currentDirection =getDirectionValue(currentDirection,"opposite");
+        }
+
+    }
+    
+    /**
+     * This method is used to get all potential available currentDirection from walker view
      * @param view reflects the view the walker has from the current position in the maze
-     *  @return all potential available direction from walker view
+     *  @return all potential available currentDirection from walker view
      * **/
     private List<Direction> judgePotentialDirections(View view){
         Direction[] directions = Direction.values();
@@ -59,20 +93,25 @@ public class Leftwalker extends Walker {
         }
         return potentialDirections;
     }
-    private void resetDirections(View view){
-        isBackWall =
+    private void checkAllCurrentWallsStatus(View view){
+        existLeftWall = !view.mayMove(getDirectionValue(currentDirection,"left"));
+        existRightWall = !view.mayMove(getDirectionValue(currentDirection,"right"));
+        existBackWall = !view.mayMove(getDirectionValue(currentDirection,"opposite"));
+        existForwardWall = !view.mayMove(currentDirection);
+
     }
 
+
    /**
-    * get the left/right direciton value of passing direction
-    * @param direction given direction
-    * @return  left/right direciton value of passing direction
+    * get the left/right direciton value of passing currentDirection
+    * @param direction given currentDirection
+    * @return  left/right direciton value of passing currentDirection
     * **/
-    private Direction getDirectionValue(Direction direction,boolean left){
+    private Direction getDirectionValue(Direction direction,String flag){
         Direction tempdirection = null;
-        get the left direciton value of passing direction
-        if(left){
-            switch (currentDirection){
+        //get the left currentDirection value of passing currentDirection
+        if(flag.equals("left")){
+            switch (direction){
                 case EAST:
                     tempdirection = Direction.NORTH;
                     break;
@@ -81,17 +120,17 @@ public class Leftwalker extends Walker {
                     break;
                 case SOUTH:
                     tempdirection = Direction.EAST;
-                    break;;
+                    break;
                 case WEST:
                     tempdirection = Direction.SOUTH;
                     break;
                 default:
-                    System.out.println("current direction does not exist");
+                    System.out.println("current currentDirection does not exist");
             }
         }
-        // get the right direciton value of passing direction
-        else{
-            switch (currentDirection){
+        // get the right currentDirection value of passing currentDirection
+        else if (flag.equals("right")){
+            switch (direction){
                 case EAST:
                     tempdirection = Direction.SOUTH;
                     break;
@@ -100,21 +139,39 @@ public class Leftwalker extends Walker {
                     break;
                 case SOUTH:
                     tempdirection = Direction.WEST;
-                    break;;
+                    break;
                 case WEST:
                     tempdirection = Direction.NORTH;
                     break;
                 default:
-                    System.out.println("current direction does not exist");
+                    System.out.println("current currentDirection does not exist");
             }
 
         }
+        // get the opposite currentDirection value of passing currentDirection
+        else {
+            switch (direction){
+                case EAST:
+                    tempdirection = Direction.WEST;
+                    break;
+                case NORTH:
+                    tempdirection = Direction.SOUTH;
+                    break;
+                case SOUTH:
+                    tempdirection = Direction.NORTH;
+                    break;
+                case WEST:
+                    tempdirection = Direction.EAST;
+                    break;
+                default:
+                    System.out.println("current currentDirection does not exist");
+            }
 
-
+        }
 
         return tempdirection;
     }
-   
+
 
 
 
