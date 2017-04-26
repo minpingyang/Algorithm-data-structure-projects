45d01ac931d149bfb07d044ed5913ac5fd38bc70
hic <minping.yang@yahoo.com>
2017-04-27 01:20:33 +1200

    done drag and onScolling methods

--- 3DRender/renderer/Renderer.java
+++ 3DRender/renderer/Renderer.java
@@ -2,6 +2,7 @@ package code.renderer;
 
 import java.awt.Color;
 import java.awt.List;
+import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelEvent;
@@ -15,33 +16,81 @@ import java.util.ArrayList;
 
 import org.omg.CORBA.PRIVATE_MEMBER;
 
-
-
 public class Renderer extends GUI {
-			//the 3D model
-			private Scene scene;
-			private Scene centralisedScene;
-			//the viewing angle for rotation
-			private float xRotation = 0f, yRotation = 0f;
-			private float rotatAngle = 0.2f;	
-			//the viewing position for translation
-			private Vector3D viewerPosition = new Vector3D(0f, 0f, 0f);
-			private float translationDistance = 2.0f;
-			//constants for zoomig
-			private float currentScale = 1.0f;
-			private static final float ZOOMING_FACTOR = 1.5f;
-			private static final float ZOOMING_MIN = 0.5f, ZOOMING_MAX = 6.0f;
-			//For rotation 
-			private boolean isRotation = true;
-			private Point dragStart;
+	// the 3D model
+	private Scene scene;
+	private Scene centralisedScene;
+	// the viewing angle for rotation
+	private float xRotation = 0f, yRotation = 0f;
+	private float rotatAngle = 0.2f;
+	// the viewing position for translation
+	private Vector3D viewerPosition = new Vector3D(0f, 0f, 0f);
+	private float translationDistance = 2.0f;
+	// constants for zoomig
+	private float currentScale = 1.0f;
+	private static final float ZOOMING_FACTOR = 1.5f;
+	private static final float ZOOMING_MIN = 0.5f, ZOOMING_MAX = 6.0f;
+	// For rotation
+	private boolean isRotation = true;
+	private Point pressStart;
+	
+	/*
+	 * Convert a 2D array of color values into a BufferedUmage.
+	 * Assumes that bitmap is indexed by column then row and 
+	 * */
+	
+	
+	
+	private void rotationX(float amount) {
+		xRotation += amount;
+	}
+
+	private void rotationY(float amount) {
+		yRotation += amount;
+	}
+
+	private void moveDown(float amount) {
+		viewerPosition = viewerPosition.plus(new Vector3D(0f, amount, 0f));
+	}
+
+	private void moveRight(float amount) {
+		viewerPosition = viewerPosition.plus(new Vector3D(amount, 0f, 0f));
+	}
+
+	
+	@Override
+	protected void onPressed(MouseEvent e) {
+		pressStart = e.getPoint();
+	}
+
+	@Override
+	protected void onReleased(MouseEvent e) {
+		Point releaseEnd = e.getPoint();
+		int offset_x= releaseEnd.x - pressStart.x;
+		int offset_y = releaseEnd.y - pressStart.y;
+		if (isRotation) {
+			rotationY(-offset_x / 100.0f);
+			rotationX(offset_y / 100.0f);
+		}else {
+			moveRight(offset_x);
+			moveDown(offset_y);
+		}
+	}
+	@Override
+	protected void onDefalut() {
+		xRotation = 0f;
+		yRotation = 0f;
+		viewerPosition = new Vector3D(0f, 0f, 0f);
+		currentScale = 1.0f;
+
+	}
+	/*
+	 * This method should parse the given file into a Scene object, which
+	 * you store and use to render an image.
+	 */
 	@Override
 	protected void onLoad(File file) {
-		// TODO fill this in.
-
-		/*
-		 * This method should parse the given file into a Scene object, which
-		 * you store and use to render an image.
-		 */
+		
 		xRotation = yRotation = 0f;
 		viewerPosition = new Vector3D(0f, 0f, 0f);
 		currentScale = 1.0f;
@@ -52,25 +101,25 @@ public class Renderer extends GUI {
 		BufferedReader bufferReader;
 		try {
 			bufferReader = new BufferedReader(new FileReader(file));
-			String  line = bufferReader.readLine();
+			String line = bufferReader.readLine();
 			if (line == null) {
 				System.out.println("The file is empty");
 				bufferReader.close();
 				return;
 			}
 			String[] values = line.split(" ");
-			//parse the light source
-			float x =Float.parseFloat(values[0]);
-			float y =Float.parseFloat(values[1]);
-			float z =Float.parseFloat(values[2]);
+			// parse the light source
+			float x = Float.parseFloat(values[0]);
+			float y = Float.parseFloat(values[1]);
+			float z = Float.parseFloat(values[2]);
 			lightPosition = new Vector3D(x, y, z);
-			
-			line =bufferReader.readLine();
-			
-			//parse polygons one by one
-			while(line != null){
+
+			line = bufferReader.readLine();
+
+			// parse polygons one by one
+			while (line != null) {
 				values = line.split(" ");
-				//coordinators of polygon
+				// coordinators of polygon
 				float aX = Float.parseFloat(values[0]);
 				float aY = Float.parseFloat(values[1]);
 				float aZ = Float.parseFloat(values[2]);
@@ -80,40 +129,36 @@ public class Renderer extends GUI {
 				float cX = Float.parseFloat(values[6]);
 				float cY = Float.parseFloat(values[7]);
 				float cZ = Float.parseFloat(values[8]);
-				float[] points = new float[]{aX,aY,aZ,bX,bY,bZ,cX,cY,cZ};
-				
+				float[] points = new float[] { aX, aY, aZ, bX, bY, bZ, cX, cY, cZ };
+
 				int surfaceRed = Integer.parseInt(values[9]);
 				int surfaceGreen = Integer.parseInt(values[10]);
 				int surfaceBlue = Integer.parseInt(values[11]);
-				int[] color = new int[]{surfaceRed,surfaceGreen,surfaceBlue};
-				polygons.add(new Polygon(points,color));
-				line = bufferReader.readLine();			
+				int[] color = new int[] { surfaceRed, surfaceGreen, surfaceBlue };
+				polygons.add(new Polygon(points, color));
+				line = bufferReader.readLine();
 			}
 			bufferReader.close();
-			this.scene = new Scene(polygons, lightPosition);			
-			
+			this.scene = new Scene(polygons, lightPosition);
+
 		} catch (FileNotFoundException e) {
 			System.err.println("FileNotFoundException");
-		}catch (IOException e) {
+		} catch (IOException e) {
 			System.err.println("IOeException");
 		}
-		
+
 	}
+
 	
-    private void rotationX(float amount) {
-		xRotation += amount;
+	private void zoom(float scale) {
+		currentScale *= scale;
+		if (currentScale > ZOOMING_MAX) {
+			currentScale = ZOOMING_MAX;
+		} else if (currentScale < ZOOMING_MIN) {
+			currentScale = ZOOMING_MIN;
+		}
 	}
-    private void rotationY(float amount) {
-		yRotation += amount;
-	}
-    private void moveDown(float amount) {
-		viewerPosition = viewerPosition.plus(new Vector3D(0f,amount, 0f));
-	}
-    private void moveRight() {
-		
-	}
-    
-    
+
 	/*
 	 * This method should be used to rotate the user's viewpoint.
 	 */
@@ -164,32 +209,21 @@ public class Renderer extends GUI {
 	@Override
 	protected void onScroll(MouseWheelEvent e) {
 		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	protected void onPressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	protected void onReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+		int i = e.getWheelRotation();
+		if (i < 0) {
+			// zoom out
+			zoom(ZOOMING_FACTOR);
+		} else {
+			zoom(1.0f / ZOOMING_FACTOR);
+		}
 	}
 
 	@Override
 	protected void switchMoveRotation() {
-		// TODO Auto-generated method stub
-		
+		isRotation =! isRotation;
 	}
 
-	@Override
-	protected void onDefalut() {
-		// TODO Auto-generated method stub
-		
-	}
+	
 }
 
 // code for comp261 assignments
