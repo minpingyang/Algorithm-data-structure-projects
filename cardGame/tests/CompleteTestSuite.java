a873f079601ccf04c4c2a1ac7fedfd93406dee61
hic <minping.yang@yahoo.com>
2017-05-05 17:51:46 +1200

    start new game called  cardGame

---
+++ cardGame/tests/CompleteTestSuite.java
@@ -0,0 +1,14 @@
+package swen221.cardgame.cards.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        swen221.cardgame.cards.tests.Part1.class,
+        swen221.cardgame.cards.tests.Part2.class,
+        swen221.cardgame.cards.tests.Part3.class,
+        swen221.cardgame.cards.tests.Part4.class
+})
+public class CompleteTestSuite {
+}
