0d2ff95698c76579350ecfc59c29d9223616a649
hic <minping.yang@yahoo.com>
2017-05-10 23:49:33 +1200

    make modified the player class by adding previous direction method, impove illegalMove by adding default serialize value

--- cardGame/core/Player.java
+++ cardGame/core/Player.java
@@ -1,5 +1,7 @@
 package swen221.cardgame.cards.core;
 
+import java.io.Serializable;
+
 /**
  * Represents a player on the board, which can be either a computer player or a
  * human. Every player has a direction (North, East, South, West) and a hand of
@@ -8,8 +10,10 @@ package swen221.cardgame.cards.core;
  * @author David J. Pearce
  * 
  */
-public class Player {
+public class Player implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
+
 	/**
 	 * Represents one of the four position on the table (North, East, South and
 	 * West).
@@ -35,6 +39,22 @@ public class Player {
 			if (this.equals(SOUTH)) return WEST;
 			return NORTH;
 		}
+		/**
+         * Returns the previous direction to play before this one (i.e.
+         * following a anti-clockwise rotation).
+         * 
+         * @return --- the previous direction
+         */
+		public Direction previous() {
+            if (this.equals(NORTH))
+                return WEST;
+            if (this.equals(EAST))
+                return NORTH;
+            if (this.equals(SOUTH))
+                return EAST;
+            return SOUTH;
+        }
+		
 	}
 
 	public final Direction direction;
