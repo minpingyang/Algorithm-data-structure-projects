63c3100ff674d1fa9f3b951702abaf7c4db266dc
hic <minping.yang@yahoo.com>
2017-05-28 16:48:33 +1200

    implement the methods addRowSafe()

--- dataBaseProject/com/bytebach/impl/MyDatabase.java
+++ dataBaseProject/com/bytebach/impl/MyDatabase.java
@@ -128,6 +128,140 @@ public class MyDatabase implements Database {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	/**
+     * This method does a series of checks to ensure that adding the given row doesn't violate the
+     * database constraints. If it does, an InvalidOperation is thrown.
+     * 
+     * @param myRowList
+     *            --- The list of rows (table) that need to add the given row in.
+     * @param element
+     *            --- The given row
+     * @throws InvalidOperation
+     *             --- if it's not safe to add.
+     */
+	public static void checkSafeToAddRow(MyRowList myRowList, List<Value> element) throws InvalidOperation {
+		// the added row must have fewer or same amount of values than schema
+        List<Field> schema = myRowList.getTable().fields();
+        if (element.size() > schema.size()) {
+            throw new InvalidOperation("Cannot add a row with more fields than schema.");
+        }
+
+        // the added row cannot have empty key field.
+        List<Integer> keyColumns = myRowList.getTable().getKeyColumns();
+        for (Integer index : keyColumns) {
+            if (element.size() < index + 1 || element.get(index) == null) {
+                throw new InvalidOperation("the added row cannot have empty key field.");
+            }
+        }
+
+        // the added row must correspond to the types determined in schema
+        for (int i = 0; i < element.size(); i++) {
+            Value value = element.get(i);
+            Field field = schema.get(i);
+
+            if (!MyDatabase.checkTypeMatch(field, value)) {
+                throw new InvalidOperation(
+                        "The added row must correspond to the types determined in schema.");
+            }
+        }
+
+        // if there are reference values in this row, check if they are good values.
+        List<ReferenceValue> refValues = referenceValues(element);
+        Database database = myRowList.getTable().getDatabase();
+        for (ReferenceValue value : refValues) {
+            MyDatabase.checkValidReference(database, value);
+        }
+
+        // the added row must have unique key value.
+        for (List<Value> row : myRowList) {
+            boolean sameKeys = true;
+            for (Integer index : keyColumns) {
+                Value existingValue = row.get(index);
+                Value value = element.get(index);
+                if (!value.equals(existingValue)) {
+                    sameKeys = false;
+                }
+            }
+            if (sameKeys) {
+                throw new InvalidOperation("The given row does not have unique key field.");
+            }
+        }
+
+	}
+	/**
+     * This method checks whether a given reference value is valid in the given database.
+     * 
+     * @param database
+     * @param reference
+     */
+	private static void checkValidReference(Database database, ReferenceValue reference) {
+		 MyTable referencedTable = null;
+
+	        // The referenced table must exist
+	        Collection<? extends Table> tables = database.tables();
+	        boolean noSuchTable = true;
+	        for (Table table : tables) {
+	            if (table.name().equals(reference.table())) {
+	                noSuchTable = false;
+	                referencedTable = (MyTable) table; // It's safe to cast
+	                break;
+	            }
+	        }
+	        if (noSuchTable) {
+	            throw new InvalidOperation("The referenced table doesn't exist.");
+	        }
+
+	        // The referenced table must have exact same schema as specified in this reference
+	        List<Integer> keyColumns = referencedTable.getKeyColumns();
+	        Value[] keys = reference.keys();
+
+	        if (keyColumns.size() != keys.length) { // check size of keys
+	            throw new InvalidOperation("Keys in this reference doesn't match the referenced table.");
+	        }
+
+	        List<Field> fields = referencedTable.fields();
+	        for (int i = 0; i < keys.length; i++) { // check type of keys
+	            if (!MyDatabase.checkTypeMatch(fields.get(keyColumns.get(i)), keys[i])) {
+	                throw new InvalidOperation("Keys in this reference doesn't match the referenced table.");
+	            }
+	        }
+
+	        // The referenced table must have such a row that matches keys specified in this reference
+	        List<List<Value>> rows = referencedTable.rows();
+	        for (List<Value> row : rows) {
+	            boolean found = true;
+	            for (int i = 0; i < keyColumns.size(); i++) {
+	                if (!row.get(keyColumns.get(i)).equals(keys[i])) {
+	                    found = false;
+	                    break;
+	                }
+	            }
+	            if (found) { // found one
+	                return;
+	            }
+	        }
+
+	        // no match
+	        throw new InvalidOperation("Found no match to this reference value.");
+		
+	}
+	/**
+     * This helper method collects all reference values in the given row.
+     * 
+     * @param element
+     *            --- the row that need to collect reference values from
+     * @return --- all reference value as a List
+     */
+	private static List<ReferenceValue> referenceValues(List<Value> element) {
+		 List<ReferenceValue> list = new ArrayList<>();
+	        for (Value value : element) {
+	            if (value instanceof ReferenceValue) {
+	                list.add((ReferenceValue) value);
+	            }
+	        }
+	        return list;
+	}
     
     
     
